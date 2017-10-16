20e208c8c639b39af82dea3ba32182c45cf58bd8
elpinal <elpinal@users.noreply.github.com>
2017-10-16 22:39:17 +0900

    Add parseTag

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,15 @@
 module Lib where
 
+import Text.Parsec
+import Text.Parsec.String
+
 data HTML =
     Elem String
   | Children [HTML]
+
+parseTag :: Parser String
+parseTag = do
+  char '<'
+  s <- many $ noneOf ">"
+  char '>'
+  return s
