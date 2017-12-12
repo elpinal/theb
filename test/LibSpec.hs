6dada446f43ee63159996b291068fd90e707bbc5
elpinal <elpinal@users.noreply.github.com>
2017-12-12 23:45:25 +0900

    Fix test

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -10,17 +10,17 @@ spec :: Spec
 spec = do
   describe "parseHTML" $
     it "parses HTML" $ do
-      parse' parseHTML "<!DOCTYPE html><html>aaa</html>" `shouldBe` Right (Node "html" [Text "aaa"])
-      parse' parseHTML "<!DOCTYPE html><html><head></head><body></body></html>" `shouldBe` Right (Node "html" [Node "head" [], Node "body" []])
+      parse' parseHTML "<!DOCTYPE html><html>aaa</html>" `shouldBe` Right (Node "html" [] [Text "aaa"])
+      parse' parseHTML "<!DOCTYPE html><html><head></head><body></body></html>" `shouldBe` Right (Node "html" [] [Node "head" [] [], Node "body" [] []])
 
   describe "parseElement" $ do
     it "parses an element" $ do
-      parse' parseElement "<html>aaa</html>" `shouldBe` Right (Node "html" [Text "aaa"])
-      parse' parseElement "<html><head></head><body></body></html>" `shouldBe` Right (Node "html" [Node "head" [], Node "body" []])
-      parse' parseElement "<html><head><title>aaa</title></head><body></body></html>" `shouldBe` Right (Node "html" [Node "head" [Node "title" [Text "aaa"]], Node "body" []])
+      parse' parseElement "<html>aaa</html>" `shouldBe` Right (Node "html" [] [Text "aaa"])
+      parse' parseElement "<html><head></head><body></body></html>" `shouldBe` Right (Node "html" [] [Node "head" [] [], Node "body" [] []])
+      parse' parseElement "<html><head><title>aaa</title></head><body></body></html>" `shouldBe` Right (Node "html" [] [Node "head" [] [Node "title" [] [Text "aaa"]], Node "body" [] []])
 
     it "parses a void element" $ do
-      parse' parseElement "<meta>" `shouldBe` Right (Node "meta" [])
+      parse' parseElement "<meta>" `shouldBe` Right (Node "meta" [] [])
 
     context "when given an invalid input" $
       it "fails" $ do
