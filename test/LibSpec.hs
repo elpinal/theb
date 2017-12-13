a32dfb870cc612f7c70969fc7b2983e6f202cfe2
elpinal <elpinal@users.noreply.github.com>
2017-12-13 17:36:10 +0900

    Align `shouldBe`

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -10,13 +10,13 @@ spec :: Spec
 spec = do
   describe "parseHTML" $
     it "parses HTML" $ do
-      parse' parseHTML "<!DOCTYPE html><html>aaa</html>" `shouldBe` Right (Node "html" [] [Text "aaa"])
+      parse' parseHTML "<!DOCTYPE html><html>aaa</html>"                        `shouldBe` Right (Node "html" [] [Text "aaa"])
       parse' parseHTML "<!DOCTYPE html><html><head></head><body></body></html>" `shouldBe` Right (Node "html" [] [Node "head" [] [], Node "body" [] []])
 
   describe "parseElement" $ do
     it "parses an element" $ do
-      parse' parseElement "<html>aaa</html>" `shouldBe` Right (Node "html" [] [Text "aaa"])
-      parse' parseElement "<html><head></head><body></body></html>" `shouldBe` Right (Node "html" [] [Node "head" [] [], Node "body" [] []])
+      parse' parseElement "<html>aaa</html>"                                          `shouldBe` Right (Node "html" [] [Text "aaa"])
+      parse' parseElement "<html><head></head><body></body></html>"                   `shouldBe` Right (Node "html" [] [Node "head" [] [], Node "body" [] []])
       parse' parseElement "<html><head><title>aaa</title></head><body></body></html>" `shouldBe` Right (Node "html" [] [Node "head" [] [Node "title" [] [Text "aaa"]], Node "body" [] []])
 
     it "parses a void element" $ do
